fbf1d74011b030ab1c4a7a78b7b208d775708a2c
Ibrahim Khalouaoui <ibrahim.khalouaoui@ADDIXO.LOCAL>
2021-07-25 21:59:29 +0100

    clean

--- src/main/java/primeur/back/entities/User.java
+++ src/main/java/primeur/back/entities/User.java
@@ -1,109 +1,102 @@
 package primeur.back.entities;
 
-import com.fasterxml.jackson.annotation.*;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-import javax.validation.constraints.Email;
-import java.io.Serializable;
-import java.util.List;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonIdentityInfo(generator= ObjectIdGenerators.IntSequenceGenerator.class)
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class)
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
 public class User implements Serializable {
 
-    @Id
-    @GeneratedValue
-    private Long id;
-    private String nom;
-    private String prenom;
-    private String mail;
-    private String password;
-    private String fonction;
-    private String equipe;
-    @Lob
-    private byte[] photo;
-    @OneToMany(mappedBy ="user")
-    private List<TechSkills> techSkills ;
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String nom;
+	private String prenom;
+	private String mail;
+	private String password;
+	private String fonction;
+	private String equipe;
+	@Lob
+	private byte[] photo;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
 
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
 
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public void setFonction(String fonction) {
-        this.fonction = fonction;
-    }
+	public void setFonction(String fonction) {
+		this.fonction = fonction;
+	}
 
-    public void setEquipe(String equipe) {
-        this.equipe = equipe;
-    }
+	public void setEquipe(String equipe) {
+		this.equipe = equipe;
+	}
 
-    public void setPhoto(byte[] photo) {
-        this.photo = photo;
-    }
+	public void setPhoto(byte[] photo) {
+		this.photo = photo;
+	}
 
+	public Long getId() {
+		return id;
+	}
 
-    public void setTechSkills(List<TechSkills> techSkills) {
-        this.techSkills = techSkills;
-    }
+	public String getNom() {
+		return nom;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public String getPrenom() {
+		return prenom;
+	}
 
-    public String getNom() {
-        return nom;
-    }
+	public String getMail() {
+		return mail;
+	}
 
-    public String getPrenom() {
-        return prenom;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public String getMail() {
-        return mail;
-    }
+	public String getFonction() {
+		return fonction;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getEquipe() {
+		return equipe;
+	}
 
-    public String getFonction() {
-        return fonction;
-    }
+	public byte[] getPhoto() {
+		return photo;
+	}
 
-    public String getEquipe() {
-        return equipe;
-    }
-
-    public byte[] getPhoto() {
-        return photo;
-    }
-
-    
-    public List<TechSkills> getTechSkills() {
-        return techSkills;
-    }
 }
